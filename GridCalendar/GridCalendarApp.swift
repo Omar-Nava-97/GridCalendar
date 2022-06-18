6224d37f8328d0a7d2f7aef520cfa62cd4b8f3d7
Omar Nava <omar.mac.97@gmail.com>
2022-06-18 14:07:55 -0500

    Initial Commit

---
+++ GridCalendar/GridCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridCalendarApp.swift
+//  GridCalendar
+//
+//  Created by Ricardo Omar Martinez Nava on 18/06/22.
+//
+
+import SwiftUI
+
+@main
+struct GridCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
